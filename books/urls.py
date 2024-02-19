59ae8f7cb360e20e41470e74d131a5b5bf69fc91
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-02-18 21:55:46 -0300

    Adição da class base view List view adicionando a função de listar todos os livros do banco de dados

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,5 @@ from books.views import *
 
 urlpatterns = [
     path("", home_page, name="home_page"),
+    path("listar", BookListView.as_view(), name="get_books"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from books.models import Book
 
 def home_page(request):
     return render(request, "books/home_page.html")
+
+class BookListView(ListView):
+    model = Book
